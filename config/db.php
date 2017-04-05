e4eff6f2db3d7bc5b81f24f9e04ed9b6a5e4252b
windows7 <e@mail>
2017-04-05 18:04:37 +0500

    adjust dbname

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=db_bludo',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=bludoapp',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
